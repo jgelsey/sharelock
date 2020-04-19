52c02ca744b467cdabf3bddff4d96daa7067b437
jgelsey <github@gelsey.com>
2020-04-18 22:29:49 -0700

    If FORCE_HTTPS then run httpsServer

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 [
 'CURRENT_KEY',
 'AUTH0_CALLBACK',
@@ -8,7 +7,9 @@ require('dotenv').config();
 'AUTH0_DOMAIN',
 'AUTH0_CLIENT_ID',
 'AUTH0_CLIENT_SECRET',
-'COOKIE_SECRET'
+'COOKIE_SECRET',
+'FORCE_HTTPS',
+'PORT'
 ].forEach(function (v) { 
     require('assert').ok(process.env[v] !== undefined, v + ' environment variable not set.'); 
 });
@@ -16,10 +17,30 @@ require('dotenv').config();
 var logger = require('./logger')
     , app = require('./app');
 
+const https = require('https');
+const fs = require('fs');
+
 app.set('port', process.env.PORT || 3000);
 
-logger.info({ port: app.get('port') }, 'setting up HTTP listener');
-var server = app.listen(app.get('port'), function (error) {
-    if (error) throw error;
-    logger.info({ port: app.get('port'), current_key: process.env.CURRENT_KEY }, 'listening');
-});
+if(process.env.FORCE_HTTPS==1) {
+	// serve the API with signed certificate on 443 (SSL/HTTPS) port
+	const httpsServer = https.createServer({
+	  key: fs.readFileSync('server.key'),       //('/etc/letsencrypt/live/my_api_url/privkey.pem'),
+	  cert: fs.readFileSync('server.cert')		 //('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
+	}, app);
+
+	logger.info({ port: app.get('port') }, 'setting up secure HTTPS listener');
+	// var server = app.listen(app.get('port'), function (error) {
+	var server = httpsServer.listen(app.get('port'), function (error) {
+	    if (error) throw error;
+	    logger.info({ port: app.get('port'), current_key: process.env.CURRENT_KEY }, 'listening');
+	});
+} 
+else {
+	logger.info({ port: app.get('port') }, 'setting up HTTP listener');
+	// var server = app.listen(app.get('port'), function (error) {
+	var server = app.listen(app.get('port'), function (error) {
+	    if (error) throw error;
+	    logger.info({ port: app.get('port'), current_key: process.env.CURRENT_KEY }, 'listening');
+	});
+}
